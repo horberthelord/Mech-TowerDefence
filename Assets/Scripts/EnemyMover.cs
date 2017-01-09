588b04295d97336a607a0b317e61692aabed2a9c
horberthelord <ipalmer@mr.preble.com>
2017-01-09 16:04:28 -0500

    dsfsndf

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.position -= Vector3.forward * 1 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.position -= Vector3.back * 1 * Time.deltaTime;
+            { }
+        }
+    }
+}
+            
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position -= Vector3.forward * 1 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+           transform.position -= Vector3.back * 1 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 relativePos = target.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(relativePos);
+        }
+    }
+
+}
+
